e042277c5927bb81de19413cad4a962352cc61ea
Jonathan Rayback <jonathan.rayback@evernym.com>
2020-05-28 18:29:19 -0600

    Small change to app message.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App () {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          I have all the beans...
+          I have all them beans...
         </p>
         <a
           className='App-link'
